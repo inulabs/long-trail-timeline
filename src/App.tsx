fdf43bcce97e79bd299ea5e8080dda10ab648a09
John Need <john.need@inulabs.tech>
2023-03-10 17:25:15 -0500

    feat: map

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import "leaflet/dist/leaflet.css";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import {LatLngExpression} from "leaflet";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    const position = [44.130838, -72.672734] as LatLngExpression
+
+    return (
+        <div className={"map-container"}>
+      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+          <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position}>
+              <Popup>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+          </Marker>
+      </MapContainer>
+        </div>
+
   );
 }
 
